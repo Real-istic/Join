524f195922e65ecbf097de5c72b5b6edbd6d02d5
Real-istic <121061694+Real-istic@users.noreply.github.com>
2023-02-23 21:55:59 +0100

    started with addTask - of work to do

--- js/add_task.js
+++ js/add_task.js
@@ -1,1 +1,20 @@
-fghhfg
+function Task(){
+    contentDiv.innerHTML = insertTask();
+}
+
+function insertTask(){
+    return /*html*/ `
+        <form class="task-left">
+            <input required type="text" name="" id="taskTitle">
+            <select name="" id="taskContacts">
+                <option value="">${userList[0].name}</option>
+                <option value="">placeholder1</option>
+                <option value="">placeholder2</option>
+            </select>
+        </form>
+        <div class="task-right">
+            <button onclick="clear()"></button>
+            <button onclick="addTask()"></button>
+        </div>
+    `;
+}
