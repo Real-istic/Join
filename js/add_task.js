261b2e07afadafafed7c367f3468a7c5d3f8fb6e
Daniel Bergmann <105534792+Tituzzz2022@users.noreply.github.com>
2023-02-24 10:09:07 +0100

    addTask

--- js/add_task.js
+++ js/add_task.js
@@ -4,17 +4,20 @@ function Task(){
 
 function insertTask(){
     return /*html*/ `
+    <div class="task-main">
+    
         <form class="task-left">
             <input required type="text" name="" id="taskTitle">
             <select name="" id="taskContacts">
-                <option value="">${userList[0].name}</option>
+                <option value="">${userList}</option>
                 <option value="">placeholder1</option>
                 <option value="">placeholder2</option>
             </select>
         </form>
         <div class="task-right">
-            <button onclick="clear()"></button>
-            <button onclick="addTask()"></button>
+            <button class="btn-clear" onclick="clear()">Create</button>
+            <button class="btn-addTask" onclick="addTask()">Create Task</button>
         </div>
+    </div>
     `;
 }
