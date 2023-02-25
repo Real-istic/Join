46703e981b0564eb9cd24276a10ca368d5638d58
Real-istic <121061694+Real-istic@users.noreply.github.com>
2023-02-25 16:12:46 +0100

    add task due date added, smooth arrow animation added

--- js/add_task.js
+++ js/add_task.js
@@ -1,29 +1,39 @@
 
 let taskClipboard = [
     {
-        'title' : 0,
+        'title': 0,
         'contacts': [],
-        'date':0,
-        'categories':[],
-        'prioritie':0,
-        'description':0,
-        'subtasks':[],
+        'date': 0,
+        'categories': [],
+        'prioritie': 0,
+        'description': 0,
+        'subtasks': [],
     }
 ]
 
 let contactListExpanded = false;
 
+/**
+ * call the add task page
+ * 
+ */
 function Task() {
     contentDiv.innerHTML = insertTask();
     document.getElementById("help").classList.remove("help-none");
 }
 
+/**
+ * inserts the add task content
+ * 
+ * @returns the html part of the page
+ */
 function insertTask() {
     return /*html*/ `
     <div class="task-main">
         <form class="task-left">
             ${insertTaskTitleHTML()}
             ${insertTaskContactlistHTML()}
+            ${insertDueDateHTML()}
             <!--more to do -->
         </form>
         <div class="task-right">
@@ -39,126 +49,159 @@ function insertTask() {
 
 }
 
-function insertTaskTitleHTML(){
+/**
+ * inserts the title of the add task page
+ * 
+ * @returns the html part of the title
+ */
+function insertTaskTitleHTML() {
     return /*html*/ `
         <input class="add-task-input-title" placeholder="Enter a title" required type="text" name="" id="taskTitle">
     `;
 }
 
-function insertTaskContactlistHTML(){
+/**
+ * inserts the contact list of the add task page
+ * 
+ * @returns the html part of the contact list
+ */
+function insertTaskContactlistHTML() {
     return /*html*/ `
-        <div class="add-task-contacts-assign" id="taskContacts">
+            <div class="add-task-contacts-assign" id="taskContacts">
                 <input required class="add-task-select-contacts" placeholder="Select Contacts to assign" onkeyup="searchContacts()" id="addContactToTaskInput">
-                <img src="assets/img/dropdownicon.svg" onclick="openAndCloseContactList()">
+                <img class="rotate-arrow-90" id="addTaskExpandArrow" src="assets/img/dropdownicon.svg" onclick="openAndCloseContactList()">
             </div>
             <div id="addTaskContactList" class="add-task-contact-list">
             </div>
     `;
 }
+/**
+ * inserts the due date of the add task page
+ * 
+ * @returns the html part of the due date
+ */
+function insertDueDateHTML() {
+    return /*html*/ `
+        <div class="add-task-date">
+            <span>Due Date</span>
+            <div class="add-task-date-form">
+                <input class="add-task-input-date" id="addTaskInputDate" type="text" required minlength="10" maxlength="10" placeholder="dd/mm/yyyy" name="" id="">
+                <img src="assets/img/calendariconl.svg" alt="">
+            </div>
+        </div>
+    `;
+}
 
 //ADD CONTACT TO TASK
 
-function searchContacts(){
- checkForExpandedContactList();
+function searchContacts() {
+    checkForExpandedContactList();
 
- let input = document.getElementById('addContactToTaskInput');
- let filter = input.ariaValueMax.toLowerCase();
+    let input = document.getElementById('addContactToTaskInput');
+    let filter = input.ariaValueMax.toLowerCase();
 
- let contacts = document.getElementsByClassName('contact');
+    let contacts = document.getElementsByClassName('contact');
 
- for (let i = 0; i < contacts.length; i++) {
-   if (contacts[i].innerText.toLowerCase().includes(filter)){
-     contacts[i].style.display = 'flex';        
-   } else{
-     contacts[i].style.display = 'none';
-   }        
- }
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].innerText.toLowerCase().includes(filter)) {
+            contacts[i].style.display = 'flex';
+        } else {
+            contacts[i].style.display = 'none';
+        }
+    }
 }
 
-function checkForExpandedContactList(){
- if(contactListExpanded == false){
-     expandContactList();    
- }
+function checkForExpandedContactList() {
+    let expandArrow = document.getElementById('addTaskExpandArrow');
+    if (contactListExpanded == false) {
+        expandArrow.classList.add('rotate-arrow-90');
+        expandArrow.classList.remove('rotate-arrow-0');
+    } else {
+        expandArrow.classList.add('rotate-arrow-0');
+        expandArrow.classList.remove('rotate-arrow-90');
+    }
 }
 
-function openAndCloseContactList(){
-    if(contactListExpanded == false){
+function openAndCloseContactList() {
+    if (contactListExpanded == false) {
         expandContactList();
-    }else{
+    } else {
         hideContactList();
     }
 }
 
-function expandContactList(){
- contactListExpanded = true;    
- loadContactList();  
- document.getElementById('addTaskContactList').classList.add('expand-contact-list');
+function expandContactList() {
+    contactListExpanded = true;
+    checkForExpandedContactList()
+    loadContactList();
+    document.getElementById('addTaskContactList').classList.add('expand-contact-list');
 }
 
 
-function loadContactList(){
- for (i = 0; i < userList.length; i++){
-     document.getElementById('addTaskContactList').innerHTML += createContactAddTaskHTML(i);
- }
+function loadContactList() {
+    for (i = 0; i < userList.length; i++) {
+        document.getElementById('addTaskContactList').innerHTML += createContactAddTaskHTML(i);
+    }
 }
 
-function hideContactList(){
+function hideContactList() {
     contactListExpanded = false
+    checkForExpandedContactList()
     document.getElementById('addTaskContactList').classList.remove('expand-contact-list');
     document.getElementById('addTaskContactList').innerHTML = ``;
 }
 
-function createContactAddTaskHTML(i){
- return `
+function createContactAddTaskHTML(i) {
+    return /*html*/ `
     <li class="add-task-contact-container" onclick="toggleContactTask(${i})">
         <input class="add-task-contact-checkbox" type="checkbox">
         <label for="confirm" class="add-task-checkbox-container">${userList[i].name}</label>
     </li>
- `    
+ `
 }
 
-function toggleContactTask(i){
- let nameStillInTask = checkForContactInClipboard(i);
+function toggleContactTask(i) {
+    let nameStillInTask = checkForContactInClipboard(i);
 
- if(nameStillInTask == true){
-     removeContactFromTask(i);  
- }else{
-     addContactToTask(i);  
- }
+    if (nameStillInTask == true) {
+        removeContactFromTask(i);
+    } else {
+        addContactToTask(i);
+    }
 }
 
-function checkForContactInClipboard(i){
- let nameStillInTask = false;
-    for(k = 0; k < taskClipboard[0].contacts.length; k++){
-        if(userList[i].name == taskClipboard[0].contacts[k]){
+function checkForContactInClipboard(i) {
+    let nameStillInTask = false;
+    for (k = 0; k < taskClipboard[0].contacts.length; k++) {
+        if (userList[i].name == taskClipboard[0].contacts[k]) {
             nameStillInTask = true;
         }
-    }  
- return nameStillInTask;    
-}
-
-function removeContactFromTask(i){
- for(j = 0; j < taskClipboard[0].contacts.length; j++){
-     if(taskClipboard[0].contacts[j] == userList[i].name){
-         taskClipboard[0].contacts.splice(j,1);        
-     }
-       
- }  
-}
-
-function addContactToTask(i){
-    let newContactForTask = userList[i].name;
-    taskClipboard[0].contacts.push(newContactForTask);   
     }
+    return nameStillInTask;
+}
+
+function removeContactFromTask(i) {
+    for (j = 0; j < taskClipboard[0].contacts.length; j++) {
+        if (taskClipboard[0].contacts[j] == userList[i].name) {
+            taskClipboard[0].contacts.splice(j, 1);
+        }
+
+    }
+}
+
+function addContactToTask(i) {
+    let newContactForTask = userList[i].name;
+    taskClipboard[0].contacts.push(newContactForTask);
+}
 
 // function addContactToTask(i){
-// let newContactForTask;   
+// let newContactForTask;
 //     for(j = 0; j < userList.length; j++){
 //         if (j == i){
 //             newContactForTask = userList[i].name;
-//         }  
-//     }     
-// taskClipborad[0].contacts.push(newContactForTask);   
+//         }
+//     }
+// taskClipborad[0].contacts.push(newContactForTask);
 // }
 
 //
