10ffe82a868578a363dd5a7f45a87e127690b10e
Konrad Thiemann <konrad.gruss@t-online.de>
2023-02-25 10:10:51 +0100

    add contact code need to be tested and need to be adjusted with label

--- js/add_task.js
+++ js/add_task.js
@@ -1,5 +1,17 @@
 
-//let contactListExpanded = false;
+let taskClipborad = [
+    {
+        'title' : 0,
+        'contacts': [],
+        'date':0,
+        'categories':[],
+        'prioritie':0,
+        'description':0,
+        'subtasks':[],
+    }
+]
+
+let contactListExpanded = false;
 
 function Task() {
     contentDiv.innerHTML = insertTask();
@@ -37,9 +49,13 @@ function insertTaskContactlistHTML(){
     return /*html*/ `
         <div class="add-task-contacts-assign" id="taskContacts">
                 <input required class="add-task-select-contacts" placeholder="Select Contacts to assign" onkeyup="searchContacts()" id="addContactToTaskInput">
-                <img src="assets/img/dropdownicon.svg" onclick="expandContactList()">
+                <img src="assets/img/dropdownicon.svg" onclick="openAndCloseContactList()">
             </div>
             <div id="addTaskContactList" class="add-task-contact-list">
+                <li class="add-task-contact-container">
+                    <input class="add-task-contact-checkbox" type="checkbox">
+                    <label for="confirm" class="add-task-checkbox-container">${userList[0].name}</label>
+                </li>
                 <li class="add-task-contact-container">
                     <input class="add-task-contact-checkbox" type="checkbox">
                     <label for="confirm" class="add-task-checkbox-container">${userList[0].name}</label>
@@ -56,63 +72,110 @@ function insertTaskContactlistHTML(){
     `;
 }
 
+//ADD CONTACT TO TASK
+
+function searchContacts(){
+ checkForExpandedContactList();
+
+ let input = document.getElementById('addContactToTaskInput');
+ let filter = input.ariaValueMax.toLowerCase();
+
+ let contacts = document.getElementsByClassName('contact');
+
+ for (let i = 0; i < contacts.length; i++) {
+   if (contacts[i].innerText.toLowerCase().includes(filter)){
+     contacts[i].style.display = 'flex';        
+   } else{
+     contacts[i].style.display = 'none';
+   }        
+ }
+}
+
+function checkForExpandedContactList(){
+ if(contactListExpanded == false){
+     expandContactList();
+     contactListExpanded = true;      
+ }
+}
+
+function openAndCloseContactList(){
+    if(contactListExpanded == false){
+        expandContactList();
+    }else{
+        hideContactList();
+    }
+}
+
+function expandContactList(){
+ contactListExpanded = true;    
+ //loadContactList();  
+ document.getElementById('addTaskContactList').classList.add = 'expand-contact-list';
+}
 
 
-//function searchContacts(){
-//  checkForExpandedContactList();
-//
-//  let input = document.getElementById('addContactToTaskInput');
-//  let filter = input.ariaValueMax.toLowerCase();
-//
-//  let contacts = document.getElementsByClassName('contact');
-//
-//  for (let i = 0; i < contacts.length; i++) {
-//    if (contacts[i].innerText.toLowerCase().includes(filter)){
-//      contacts[i].style.display = 'flex';        
-//    } else{
-//      contacts[i].style.display = 'none';
-//    }        
-//  }
-//}
-//
-//function checkForExpandedContactList(){
-//  if(contactListExpanded == false){
-//      expandContactList();
-//      contactListExpanded = true;      
-//  }
-// 
-//
-//function expandContactList(){
-//  contactListExpanded = true;    
-//  loadContactList();  
-//  document.getElementById('addTaskContactList').classList.add = 'expand-contact-list';
-//}
-//
-//
-//function loadContactList(){
-//  for (i = 0; i < userList.length; i++){
-//      document.getElementById('addTaskContactList').innerHTML += createContactAddTaskHTML(i);
-//  }
-//}
-//
-//function createContaktAddTaskHTML(i){
-//  return `
-//  <label class="add-task-contact-container" id="contact${i}" onclick="addContactToTask(i)">
-//      ${userList[${i}].name}
-//      <div class="add-task-checkbox-container">
-//          <input class="add-task-contact-checkbox" type="checkbox">
-//      </div>
-//  </label>
-//  `    
-//}
-//
-//function addContactToTask(i){
-//  hideContactList();    
-//}
-//
-//function hideContactList(){
-//  document.getElementById('addTaskContactList').classList.remove = 'expand-contact-list';
-//}
+function loadContactList(){
+ for (i = 0; i < userList.length; i++){
+     document.getElementById('addTaskContactList').innerHTML += createContactAddTaskHTML(i);
+ }
+}
+
+function hideContactList(){
+    document.getElementById('addTaskContactList').classList.remove = 'expand-contact-list';
+}
+
+function createContaktAddTaskHTML(i){
+ return `
+ <label class="add-task-contact-container" id="contact${i}" onclick="toggleContactTask(i)">
+     ${userList[i].name}
+     <div class="add-task-checkbox-container">
+         <input class="add-task-contact-checkbox" type="checkbox">
+     </div>
+ </label>
+ `    
+}
+
+function toggleContactTask(i){
+ let nameStillInTask = checkForContactInClipborad(i);
+
+ if(nameStillInTask == true){
+     removeContactFromTask(i)  
+ }else{
+     addContactToTask(i)  
+ }
+}
+
+function checkForContactInClipboard(i){
+
+ let nameStillInTask = false;
+   
+ for(j = 0; j < userList.length; j++){
+     for(k = 0; k < taskClipborad.contacts.length; k++){
+         if(userList[i].name == taskClipboard.contacts[k]){
+             nameStillInTask = true;
+         }
+     }  
+ }
+ return nameStillInTask;    
+}
+
+function removeContactFromTask(i){
+ for(j = 0; j < taskClipboard.contacts.length; j++){
+     if(taskClipboard.contacts[j] == userList[i].name){
+         taskClipboard.contacts.splice(j,1);        
+     }
+       
+ }  
+}
+
+function addContactToTask(i){
+ for(j = 0; j < userList.length; j++){
+     if (j == i){
+         let newContactForTask = userList[i].name;
+     }  
+ }  
+ taskClipborad['contacts'].push(newContactForTask);   
+}
+
 //
 // ----- ACHTUNG CSS -------
 //.expand-contact-list{
