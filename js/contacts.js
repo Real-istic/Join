8edb41bf6ff9a807313cc3a99878e2b8c6e4fed8
Daniel Bergmann <105534792+Tituzzz2022@users.noreply.github.com>
2023-02-26 19:12:57 +0100

    contact List reload

--- js/contacts.js
+++ js/contacts.js
@@ -1,10 +1,20 @@
-
 /**
  * 
  * call the contacts content
  */
 function insertContacts() {
-    contentDiv.innerHTML = contactsHTML();
+    contentDiv.innerHTML = `
+        <div class="contact-main">
+            <div class="contact-left">
+                ${renderUserList()};
+            </div>
+
+            <div class="contact-right">
+                <div class="new-contact-button" onclick="">New Contact</div>
+            </div>
+        </div> 
+    `;
+        
     document.getElementById("help").classList.remove("help-none");
 }
 
@@ -13,81 +23,47 @@ function insertContacts() {
  * 
  * @returns the html part
  */
-function contactsHTML() {
-    let firstLetters = [];
-    for (let i = 0; i < userList.length; i++) {
-        const firstLetter = userList[i].name.charAt(0);
-        if (!firstLetters.includes(firstLetter)) {
-            firstLetters.push(firstLetter);
-        }
-    }
-    let userListHTML = "";
-    for (let i = 0; i < firstLetters.length; i++) {
-        const letter = firstLetters[i];
-        userListHTML += /*html*/ `
-            <div class="contact-letter-main">
-                <h4 class="contact-letter">${letter}</h4>
-                <div id="${letter}">
-        `;
-        for (let j = 0; j < userList.length; j++) {
-            const user = userList[j];
-            if (user.name.charAt(0) === letter) {
-                userListHTML += /*html*/ `
-                    <div class="contact-child-div" onclick="openDetailContact('${j}', '${user.name.charAt(0)}')" id="${j}">
-                        <div style="background-color: #e36d6f" class="contact-child">
-                            <p>${user.name.charAt(0)}</p>
-                        </div>
-                        <div>
-                            <p class="contact-name">${user.name}</p>
-                            <p class="contact-email">${user.email}</p>
-                        </div>
-                    </div>
-                `;
-            }
-        }
-        userListHTML += /*html*/ `
-                </div>
-            </div>
-        `;
-    }
-    return /*html*/ `
-        <div class="contact-main">
-            <div class="contact-left">
-                ${userListHTML}
-            </div>
-            <div class="contact-right">
-                <div class="new-contact-button" onclick="">New Contact</div>
-            </div>
-        </div>
-    `;
-}
-
-
-/**
- *
- * render userList
- * 
-*/
 function renderUserList() {
     let userListHTML = "";
     for (let i = 0; i < userList.length; i++) {
+        const firstNameLetter = userList[i].vorname.charAt(0);
+        const lastNameLetter = userList[i].nachname.charAt(0);
+        const contactName = firstNameLetter + lastNameLetter;
+        const randomColor = getRandomColor();
+
         userListHTML += /*html*/ `
-        <div class="contact-letter-main">
-                <h4 class="contact-letter">Test Letter </h4>
-                    <div id="Test Letter">
-                        <div class="contact-child-div" onclick="openDetailContact('0', 'BK' )" id="0">
-                            <div style="background-color: #e36d6f" class="contact-child">
-                                <p>BK</p>
-                            </div>
-                                <div>
-                                    <p class="contact-name">${userList[i].name}</p>
-                                    <p class="contact-email">${userList[i].email}</p>
-                                </div>                               
+            <div class="contact-letter-main">
+                <h4 class="contact-letter">${userList[i].vorname.charAt(0)}</h4>
+                <div id="testLetter">
+                    <div class="contact-child-div" onclick="openDetailContact('0', '${contactName}')" id="0">
+                        <div style="background-color: ${randomColor}" class="contact-child">
+                            <p>${contactName}</p>
                         </div>
+                        <div>
+                            <p class="contact-name">${userList[i].vorname} ${userList[i].nachname}</p>
+                            <p class="contact-email">${userList[i].email}</p>
+                        </div>                               
                     </div>
+                </div>
             </div>
         `;
+       
     }
+
     return userListHTML;
+
 }
 
+/**
+ * returns a random hex color code
+ * 
+ * @returns a string representing a hex color code
+ */
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
