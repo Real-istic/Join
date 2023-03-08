49d66a02bd46bd2db4b7b0c20ef7e896f03ca09a
Real-istic <121061694+Real-istic@users.noreply.github.com>
2023-03-08 20:53:51 +0100

    small things

--- js/global_variables.js
+++ js/global_variables.js
@@ -1,3 +1,6 @@
+/**
+ * the main render window
+ */
 let contentDiv = document.getElementById('content');
 
 /* if adding changes are made in this Clipboard, these changes must be made in the add_task.js, too. (function clearTask) !! */
