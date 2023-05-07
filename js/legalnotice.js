05f7e15aeff0ee8f8223e691fcbbd3d938f17346
Real-istic <RealSkaarj@gmx.de>
2023-05-07 20:08:19 +0200

    small changes

--- js/legalnotice.js
+++ js/legalnotice.js
@@ -18,7 +18,7 @@ function legalNoticeHTML(){
     <div class="legal-notice-div">
         <h1 style="margin-top: 0;">Legal Notice</h1>
         <h2>Angaben gemäß § 5 TMG</h2>
-        <p>Daniel Bergmann</p>
+        <p>Patrizio Marzullo</p>
         <h2>Kontakt</h2>
         <p>Telefon: +49 151 546 940 58</p>
     </div>
